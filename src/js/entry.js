b1e5b00cd3c7a5150770c4ccd1417addb55602b5
Jakub Šatník <jsatnik@oriflame.com>
2020-04-02 20:20:57 +0200

    Content type sections utilisation

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-console */
 
 import init from './partials/main';
-
+import lazySectionBackground from './partials/lazy-section-background';
 import lazy from './partials/lazy';
 
 import '../scss/entry.scss';
 
 init().then(() => {
+  lazySectionBackground({ elementSelector: '.shpr-lazy-background' });
   lazy({
     elementSelector: '.lazy',
     tags: ['IFRAME', 'IMG'],
